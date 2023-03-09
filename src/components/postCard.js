707cc86c694480f36991dc558c3a956c045f7e74
Marwa <marwa.ehab@kijamii.com>
2023-03-09 20:36:28 +0200

    design + add comment

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -4,11 +4,11 @@ function PostCard({ postItem }) {
   return (
     <div className="postCard">
       <Link to={`/posts/${postItem.id}`} state={{ userId: 1 }}>
-        <div>
-          <img src="imgs/post1.jpg" alt="post Picture" className="postImg" />
+        <img src="imgs/post1.jpg" alt="post Picture" className="postImg" />
+        <div className="cardContent">
+          <h2>{postItem.title}</h2>
+          <p>{postItem.body}</p>
         </div>
-        <h2>{postItem.title}</h2>
-        <p>{postItem.body}</p>
       </Link>
     </div>
   );
--- src/pages/postDetails.js
+++ src/pages/postDetails.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
-import Grid from "@mui/material/Grid";
 import { useParams, useLocation } from "react-router-dom";
 import { API } from "../constants/constants";
 import { Link } from "react-router-dom";
+import PhoneIcon from "@mui/icons-material/Phone";
+import BusinessIcon from "@mui/icons-material/Business";
+import SendIcon from "@mui/icons-material/Send";
 
 function PostDetails() {
   const [postDetails, setPostDetails] = useState([]);
@@ -11,6 +13,7 @@ function PostDetails() {
   const { id } = useParams();
   const location = useLocation();
   const userId = location.state?.userId;
+  const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
     fetch(`${API}posts/${id}`)
@@ -32,23 +35,68 @@ function PostDetails() {
       });
   }, []);
 
+  console.log("comments", comments);
+
+  const handleAddComment = (event) => {
+    fetch(`${API}posts/${id}/comments`, {
+      method: "POST",
+      body: JSON.stringify({
+        postId: 1,
+        id: "1",
+        name: "id labore ex et quam laborum",
+        email: "Eliseo@gardner.biz",
+        body: newComment,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("comment added", json);
+      });
+  };
+
   return (
-    <div className="App">
+    <div className="postDetails">
+      <img
+        src="/imgs/post1.jpg"
+        alt="post Picture"
+        className="postDetailsImg"
+      />
+
       <h1>{postDetails.title}</h1>
       <p>{postDetails.body}</p>
       <Link to={`/${userId}`} state={{ userName: userData && userData.name }}>
-        <h1>{userData && userData.name}</h1>
+        <div className="userName">
+          <img src="/imgs/user.png" alt="user Picture" className="useImg" />
+          <h3>{userData && userData.name}</h3>
+        </div>
       </Link>
-      <h4>{userData && userData.phone}</h4>
-      <h4>{userData && userData.company && userData.company.name}</h4>
+      <h4>
+        <PhoneIcon />
+        {userData && userData.phone}
+      </h4>
+      <h4>
+        <BusinessIcon />
+        {userData && userData.company && userData.company.name}
+      </h4>
       <div className="commentSection">
-        {comments.map((comment) => (
-          <div className="comment">
+        {comments.map((comment, index) => (
+          <div className="comment" key={index}>
             <img src="/imgs/user.png" alt="user Picture" className="useImg" />
-
             <p>{comment.body}</p>
           </div>
         ))}
+        <div className="commentInput">
+          <input
+            type="text"
+            placeholder="write your comment"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+          <SendIcon onClick={handleAddComment} />
+        </div>
       </div>
     </div>
   );
